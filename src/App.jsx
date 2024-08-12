2781b649fb6b57152fd2692c649d95a323d6a39c
Afshin Zarei <afshin.zarei2000@gmail.com>
2024-08-12 10:16:12 +0330

    Routes were created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/404";
 
 function App() {
-
   return (
     <>
-      <h1>Afshin Zarei</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
