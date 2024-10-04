e992ce20f4857d1ad23f892f3313a415f589a79b
Afshin Zarei <afshin.zarei2000@gmail.com>
2024-10-04 11:59:52 +0330

    Added data display in shopping cart

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -5,34 +5,33 @@ import BasketSidebar from "../components/BasketSidebar";
 import styles from "./CheckoutPage.module.css";
 
 import emptyBasket from "../assets/emptyBasket.png";
+import { useSelector } from "react-redux";
 
 function CheckoutPage() {
   // const [state, dispatch] = useCart();
 
+  const state = useSelector((store) => store.cart);
+
   // const clickHandler = (type, payload) => dispatch({ type, payload });
 
-  // if (!state.itemsCounter) {
-  //   return (
-  //     <div className={styles.empty_basket}>
-  //       <img src={emptyBasket} alt="Empty basket" />
-  //       <br />
-  //       <p>Your shopping cart is empty!</p>
-  //     </div>
-  //   );
-  // }
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.empty_basket}>
+        <img src={emptyBasket} alt="Empty basket" />
+        <br />
+        <p>Your shopping cart is empty!</p>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.container}>
-      {/* <BasketSidebar state={state} clickHandler={clickHandler} />
+      <BasketSidebar state={state} />
       <div className={styles.products}>
         {state.selectedItems.map((product) => (
-          <BasketCard
-            key={product.id}
-            data={product}
-            clickHandler={clickHandler}
-          />
+          <BasketCard key={product.id} data={product} />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
